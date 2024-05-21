f5dded1184f697f81e0f05db24f232318c2ed03a
thiago araujo <devthiagoaraujo@gmail.com>
2024-05-21 13:36:53 -0300

    feat: serializers validation and Movie rate_stars

--- actors/serializers.py
+++ actors/serializers.py
@@ -6,3 +6,9 @@ class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
         fields = '__all__'
+
+    def validate_birthday(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError(
+                'Birthday must be after 1900')
+        return value
--- movies/serializers.py
+++ movies/serializers.py
@@ -1,8 +1,31 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movies
 
 
 class MoviesSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movies
         fields = '__all__'
+
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if rate:
+            return round(rate, 1)
+
+        return None
+
+    def validate_release_date(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError(
+                'Release date must be after 1970')
+        return value
+
+    def validate_resume(self, value):
+        if len(value) > 1000:
+            raise serializers.ValidationError(
+                'Resume must have less than 1000 characters')
+        return value
