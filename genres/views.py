0dabd786d2543138aedcaaff993d342b31c1b744
thiago araujo <devthiagoaraujo@gmail.com>
2024-07-01 08:45:02 -0300

    feat: global permission

--- genres/views.py
+++ genres/views.py
@@ -2,15 +2,16 @@ from genres.models import Genre
 from rest_framework.permissions import IsAuthenticated
 from genres.serializers import GenreSerializer
 from rest_framework import generics
+from app.permissions import GlobalDefaultPermission
 
 
 class GenresListCreateView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 
 class GenresRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
--- movies/views.py
+++ movies/views.py
@@ -2,15 +2,16 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from movies.models import Movies
 from movies.serializers import MoviesSerializer
+from app.permissions import GlobalDefaultPermission
 
 
 class MoviesListCreateView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movies.objects.all()
     serializer_class = MoviesSerializer
 
 
 class MoviesRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movies.objects.all()
     serializer_class = MoviesSerializer
